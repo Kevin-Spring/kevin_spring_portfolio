3495994b21751993b15c2889d69907e4d4d9bef4
Kevin Spring <kevinspring93@hotmail.com>
2020-04-16 16:38:37 +0200

    add cv

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
-/* 3D effect for header */
+/* Copyright (c) 2020 by Vadym (https://codepen.io/vadimhimself/pen/vwgKqO)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+/* ---------3D effect for header ------------*/
 
 const text = document.querySelectorAll('.text');
 const halfX = window.innerWidth / 2;
@@ -19,9 +29,13 @@ document.addEventListener('mousemove', (e) => {
   });
 });
 
+/* ------------ END OF 3D TEXT ------------ */
 
+
+/* GSAP ANIMATIONS */
 /* Animation for the links */
-gsap.from(".section__links-item", {autoAlpha:0, duration: 1, stagger: 0.25});
+gsap.from(".stagger-animation", {autoAlpha:0, duration: 1, stagger: 0.25});
+gsap.from(".main-animation", {autoAlpha:0, duration: 1, y:150, stagger: 0.25});
 
 
 /* GSAP Scrollamgic for the links */
